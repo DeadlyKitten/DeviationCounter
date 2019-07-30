939d008287ad65f31a8b99cccdf103df7364e60b
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2019-07-30 00:05:41 -0500

    Created base for counter

---
+++ DeviationCounter/DeviationCounter.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using TMPro;
+using UnityEngine.UI;
+
+namespace DeviationCounter
+{
+    class DeviationCounter : MonoBehaviour
+    {
+        private TextMeshProUGUI _counter;
+        private float _sum;
+
+        void Start()
+        {
+            StartCoroutine(FindScoreController());
+
+            Canvas canvas = gameObject.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.WorldSpace;
+            CanvasScaler cs = gameObject.AddComponent<CanvasScaler>();
+            cs.scaleFactor = 10.0f;
+            cs.dynamicPixelsPerUnit = 10f;
+            GraphicRaycaster gr = gameObject.AddComponent<GraphicRaycaster>();
+            gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1f);
+            gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 1f);
+
+            _counter = CustomUI.BeatSaber.BeatSaberUI.CreateText(canvas.transform as RectTransform, $"Deviation \n{0}", Vector2.zero);
+            _counter.alignment = TextAlignmentOptions.Center;
+            _counter.transform.localScale *= .12f;
+            _counter.fontSize = 2.5f;
+            _counter.color = Color.white;
+            _counter.lineSpacing = -50f;
+            _counter.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 1f);
+            _counter.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 1f);
+            _counter.enableWordWrapping = false;
+            _counter.transform.localPosition = new Vector3(-0.1f, 2.5f, 8f);
+        }
+
+        IEnumerator FindScoreController()
+        {
+            yield return new WaitUntil(() => Resources.FindObjectsOfTypeAll<ScoreController>().Any());
+            yield return new WaitUntil(() => Resources.FindObjectsOfTypeAll<AudioTimeSyncController>().Any());
+
+            ScoreController scoreController = Resources.FindObjectsOfTypeAll<ScoreController>().First();
+            AudioTimeSyncController timeSyncController = Resources.FindObjectsOfTypeAll<AudioTimeSyncController>().First();
+
+            scoreController.noteWasCutEvent += OnNoteHit;
+        }
+
+        void OnNoteHit(NoteData data, NoteCutInfo info, int score)
+        {
+            _sum += info.timeDeviation;
+            
+            Logger.Log($"Note {data.id} cut with deviation: {info.timeDeviation}");
+
+            _counter.text = $"Deviation \n{Mathf.Round((_sum / (data.id - 1)) * 1000)} ms";
+        }
+    }
+}
--- DeviationCounter/Plugin.cs
+++ DeviationCounter/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using IPA;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using IPALogger = IPA.Logging.Logger;
 
@@ -6,6 +7,8 @@ namespace DeviationCounter
 {
     public class Plugin : IBeatSaberPlugin
     {
+        DeviationCounter _counter;
+
         public void Init(IPALogger logger)
         {
             Logger.logger = logger;
@@ -19,7 +22,13 @@ namespace DeviationCounter
 
         public void OnUpdate() { }
 
-        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene) { }
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
+            if (nextScene.name == "GameCore")
+            {
+                _counter = new GameObject("Deviation Counter").AddComponent<DeviationCounter>();
+            }
+        }
 
         public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode) { }
 
