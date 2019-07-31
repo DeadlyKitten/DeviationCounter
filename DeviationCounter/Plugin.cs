5e0cd7bf011d2ade56976b2ad2fb3516077fa033
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2019-07-31 12:08:15 -0500

    Added CountersPlus integration

--- DeviationCounter/Plugin.cs
+++ DeviationCounter/Plugin.cs
@@ -1,6 +1,8 @@
-﻿using IPA;
+﻿using System.Linq;
+using IPA;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using CountersPlus.Custom;
 using IPALogger = IPA.Logging.Logger;
 
 namespace DeviationCounter
@@ -14,7 +16,17 @@ namespace DeviationCounter
             Logger.logger = logger;
         }
 
-        public void OnApplicationStart() { }
+        public void OnApplicationStart()
+        {
+            Logger.Log("Checking for Counters+");
+            if (IPA.Loader.PluginManager.AllPlugins.Any(x => x.Metadata.Id == "Counters+"))
+            {
+                Logger.Log("Counters+ is installed");
+                AddCustomCounter();
+            }
+            else
+                Logger.Log("Counters+ not installed");
+        }
 
         public void OnApplicationQuit() { }
 
@@ -33,5 +45,18 @@ namespace DeviationCounter
         public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode) { }
 
         public void OnSceneUnloaded(Scene scene) { }
+
+        private void AddCustomCounter()
+        {
+            Logger.Log("Creating Custom Counter");
+            CustomCounter counter = new CustomCounter
+            {
+                SectionName = "deviationCounter",
+                Name = "Deviation Counter",
+                BSIPAMod = this,
+                Counter = "Deviation Counter",
+            };
+            CustomCounterCreator.Create(counter);
+        }
     }
 }
