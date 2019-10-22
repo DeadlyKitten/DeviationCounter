a0e2fad51db779f2ccfdaa12e9787910c3f85246
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2019-10-22 05:05:56 -0500

    Added Max Deviation limit for hit bloq.

---
+++ DeviationCounter/Patches/NoteCutInfo_AllIsOk.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+
+namespace DeviationCounter.Patches
+{
+    [HarmonyPatch(typeof(NoteCutInfo))]
+    [HarmonyPatch("allIsOK", MethodType.Getter)]
+    class NoteCutInfo_allIsOK
+    {
+        private static bool Prefix(NoteCutInfo __instance, ref bool __result)
+        {
+            __result = __instance.speedOK && __instance.directionOK && __instance.saberTypeOK && !__instance.wasCutTooSoon &&
+                !(Plugin.useMaxDeviation && (__instance.timeDeviation * 1000 > Plugin.maxDeviation));
+            return false;
+        }
+    }
+}
--- DeviationCounter/Plugin.cs
+++ DeviationCounter/Plugin.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using CountersPlus.Custom;
 using IPALogger = IPA.Logging.Logger;
+using Harmony;
+using System;
+using System.Reflection;
 
 namespace DeviationCounter
 {
     public class Plugin : IBeatSaberPlugin
     {
         DeviationCounter _counter;
+        BS_Utils.Utilities.Config _config;
+
+        internal static bool useMaxDeviation;
+        internal static int maxDeviation;
 
         public void Init(IPALogger logger)
         {
             Logger.logger = logger;
+            _config = new BS_Utils.Utilities.Config("Deviation Counter");
+            LoadConfig();
         }
 
         public void OnApplicationStart()
@@ -26,6 +35,26 @@ namespace DeviationCounter
             }
             else
                 Logger.Log("Counters+ not installed");
+
+            if (useMaxDeviation)
+            {
+                Logger.Log("Applying Harmony Patches", Logger.LogLevel.Notice);
+                try
+                {
+                    var harmony = HarmonyInstance.Create("com.steven.beatsaber.deviation.counter");
+                    harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+                    Logger.Log("Patched successfully");
+                }
+                catch (Exception e)
+                {
+                    Logger.Log($"{e.Message}\n{e.StackTrace}",
+                        Logger.LogLevel.Error,
+                        "This plugin requires Harmony. Make sure you installed the plugin " +
+                        "properly, as the Harmony DLL should have been installed with it."
+                    );
+                }
+            }
         }
 
         public void OnApplicationQuit() { }
@@ -61,10 +90,16 @@ namespace DeviationCounter
             {
                 Enabled = true,
                 Position = CountersPlus.Config.ICounterPositions.BelowCombo,
-                Index = 1
+                Distance = 1
             };
 
             CustomCounterCreator.Create(counter, defaults);
+        }
+
+        private void LoadConfig()
+        {
+            useMaxDeviation = _config.GetBool("Settings", "Use Max Deviation", false, true);
+            maxDeviation = _config.GetInt("Settings", "Max Deviation", 15, true);
         }
     }
 }
